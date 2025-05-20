706f1f63e5391532bc6ef337813751c0dd768d19
Richard Blacker <richardblacker1@outlook.com>
2025-05-19 21:39:02 -0500

    new

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://e-helpdesk-back-r1nabbk9f-richard-blackers-projects.vercel.app/',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://e-helpdesk-back-r1nabbk9f-richard-blackers-projects.vercel.app',
   },
   // GitHub Pages configuration
   basePath: process.env.NODE_ENV === 'production' ? '/E-Helpdesk-Front' : '',
