bf74d1063b89a54a90a478d3b35ef6b06db7774c
Richard Blacker <richardblacker1@outlook.com>
2025-05-20 18:38:21 -0500

    add back url

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+
+
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://e-helpdesk-back-r1nabbk9f-richard-blackers-projects.vercel.app',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://www.support-xq.org/',
   },
   images: {
     unoptimized: true,
