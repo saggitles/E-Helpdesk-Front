0280e3b3c2e0b8692efac92968cb6b2a9d8d713d
Richard Blacker <richardblacker1@outlook.com>
2025-09-15 09:58:55 -0500

    clean code 4

--- src/components/generalComponents/alerts/components/Unresolved.tsx
+++ src/components/generalComponents/alerts/components/Unresolved.tsx
@@ -26,26 +26,7 @@ function Unresolved({
 
   return (
     <>
-      <div className='flex flex-col items-center w-full px-8 py-2 text-left '>
-        {/* {unresolvedTickets.map((ticket, index) => {
-          return (
-            <UnresolvedTicket style={{ display: "flex" }} key={"UT" + index}>
-              <UnresolvedTicketFirstColumn>
-                <p>Ticket #{ticket.id}</p>
-              </UnresolvedTicketFirstColumn>
-              <UnresolvedTicketSecondColumn>
-                {ticket.status === "1" ? (
-                  <ResolvedIcon viewBox="0 0 24 24">
-                    <polyline points="20 6 9 17 4 12" />
-                  </ResolvedIcon>
-                ) : (
-                  <UnresolvedButton onClick={ _=> {setResolve(true)}} >View</UnresolvedButton>
-                )}
-              </UnresolvedTicketSecondColumn>
-            </UnresolvedTicket>
-          );
-        })} */}
-      </div>
+      <div className='flex flex-col items-center w-full px-8 py-2 text-left '></div>
 
       <div className='flex justify-center w-full'>
         <button
